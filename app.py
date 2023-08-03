58a813f9f5c9ae698791a1bba54ef43163b0b9df
Riccardo Delli Compagni <47278210+RiccardoDC@users.noreply.github.com>
2023-08-03 16:17:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,14 @@ import numpy as np
 import pandas as pd
 import openai 
 import langchain
-from PIL import Image
+
+from langchain.document_loaders import  TextLoader
+from langchain.document_loaders import DirectoryLoader
+
+from langchain.document_loaders import OnlinePDFLoader
+
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.llms import OpenAI
 
 
 
